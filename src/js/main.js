83c2ab61b6e5f0a4bb58e24a054a8172cac8f04f
damir <dimayu82@gmail.com>
2023-05-13 17:45:04 +0300

    Add styles for desktop

--- src/js/main.js
+++ src/js/main.js
@@ -8,8 +8,26 @@ const swiper = new Swiper('.swiper-home', {
 
 const swiperProduct = new Swiper('.swiper-new-product', {
   loop: true,
+  slidesPerView: 4,
+  spaceBetween: 16,
   navigation: {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
   },
 });
+
+document.querySelector('.header__items__right__link--search').addEventListener('click', () => {
+  document.querySelector('.form__search').classList.toggle('form__search--visible');
+});
+
+window.addEventListener('scroll', () => {
+  let scrollDistance = window.scrollY;
+  const header = document.querySelector('.header');
+  
+  scrollDistance > 90 ? header.classList.add('bg-visible') : header.classList.remove('bg-visible')
+});
+
+// document.querySelector('#burger').addEventListener('click', () => {
+//   document.querySelector('.nav').classList.toggle('active');
+//   document.querySelector('#burger').classList.toggle('active');
+// });
