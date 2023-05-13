e2c348f421bc4ba83f6872a875c401b22eb05de9
damir <dimayu82@gmail.com>
2023-05-13 12:49:15 +0300

    Html markup

---
+++ src/js/main.js
@@ -0,0 +1,15 @@
+const swiper = new Swiper('.swiper-home', {
+  loop: true,
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
+
+const swiperProduct = new Swiper('.swiper-new-product', {
+  loop: true,
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
